ed25421631bdc720c26e0d1da64cc9dc55b31e79
billowbashir <billowbashir@gmail.com>
2018-10-17 11:23:45 +0300

    display projects

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Project
 
 
 @login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request,'index.html')
+    projects=Project.objects.all()
+    return render(request,'index.html',{'projects':projects})
