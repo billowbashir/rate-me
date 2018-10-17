169d27af2946d50c64900940e6d6353907847e5a
billowbashir <billowbashir@gmail.com>
2018-10-17 09:24:10 +0300

    complete authentication

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include(app.urls))
+    url(r'',include('app.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
