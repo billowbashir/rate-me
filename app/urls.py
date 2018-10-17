b41bf304e82796c5316ff905ec4ba7331efa92f6
billowbashir <billowbashir@gmail.com>
2018-10-17 19:36:27 +0300

    styling

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
 urlpatterns=[
        url(r'^$',views.home,name='Home'),
        url(r'^new_project',views.new_project,name='new_project'),
         url(r'^new/profile$', views.new_profile, name='new-profile'),
         url(r'^profile/',views.profile, name='profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
