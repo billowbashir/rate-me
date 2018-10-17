fb51ab941ed2d169894f17f4cfb0f043b529aec3
billowbashir <billowbashir@gmail.com>
2018-10-17 13:05:13 +0300

    try showing immage

--- app/models.py
+++ app/models.py
@@ -6,4 +6,15 @@ class Project(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=250)
     link=models.URLField(max_length=100)
+    project_image=models.ImageField(upload_to='projects/')
     owner=models.ForeignKey(User,on_delete=models.CASCADE)
+class Profile(models.Model):
+    profile_pic=models.ImageField(upload_to='profile_photos/')
+    bio=models.CharField(max_length=300)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def save_profile(self):
+        self.save()
+    @classmethod
+    def search_by_profile(cls,search_term):
+        profiles=cls.objects.filter(user__icontains=search_term)
--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -123,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
