2e315cb1a3544457d88ee4104ca8b036c34005fd
billowbashir <billowbashir@gmail.com>
2018-10-19 18:30:36 +0300

    settings

--- project/settings.py
+++ project/settings.py
@@ -83,9 +83,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awwards',
-        'USER': 'bashir',
-        'PASSWORD':'bashiir',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
 
